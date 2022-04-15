9af2afe021e3c67fe5310c3f77677ebebdd222db
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-15 11:36:56 -0300

    initializing spring projects

---
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/AwmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stefano.api.awm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwmBackendApplication.class, args);
+	}
+
+}
---
+++ awm-backend/src/test/java/com/stefano/api/awm/backend/AwmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefano.api.awm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

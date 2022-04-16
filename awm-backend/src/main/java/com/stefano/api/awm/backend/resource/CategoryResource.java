3955a6c7f4a9559e826bb4ecc3bc6b69114efdca
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-16 14:07:47 -0300

    creating new category

--- awm-backend/src/main/java/com/stefano/api/awm/backend/resource/CategoryResource.java
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/resource/CategoryResource.java
@@ -1,10 +1,21 @@
 package com.stefano.api.awm.backend.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.stefano.api.awm.backend.model.Category;
 import com.stefano.api.awm.backend.repository.CategoryRepository;
@@ -22,6 +33,23 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> listAll() {
 		return categoryRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category saveCategory = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(saveCategory.getId()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(saveCategory);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Category> findById(@PathVariable Long id) {
+		return categoryRepository.findById(id);
 	}
 
 }

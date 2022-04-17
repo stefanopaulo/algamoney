56d2afd1d8d22ae7d2641005d3514f870de304ed
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-17 10:04:41 -0300

    removing people with delete

--- awm-backend/src/main/java/com/stefano/api/awm/backend/exceptionhandler/AlgamoneyExceptionHandler.java
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -47,6 +49,16 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Error> errors = createErrorsList(ex.getBindingResult());
 		
 		return handleExceptionInternal(ex, errors, headers, status, request);
+	}
+	
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		String msgUser = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String msgDeveloper = ex.toString();
+		
+		List<Error> errors = Arrays.asList(new Error(msgUser, msgDeveloper));
+		
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	private List<Error> createErrorsList(BindingResult bindingResult) {
--- awm-backend/src/main/java/com/stefano/api/awm/backend/resource/PersonResources.java
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/resource/PersonResources.java
@@ -9,11 +9,13 @@ import javax.validation.Valid;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stefano.api.awm.backend.event.ResourceCreatedEvent;
@@ -50,6 +52,12 @@ public class PersonResources {
 	public ResponseEntity<Optional<Person>> findById(@PathVariable Long id) {
 		Optional<Person> person = personRepository.findById(id);
 		return !person.isEmpty() ? ResponseEntity.ok(person) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		personRepository.deleteById(id);
 	}
 
 }

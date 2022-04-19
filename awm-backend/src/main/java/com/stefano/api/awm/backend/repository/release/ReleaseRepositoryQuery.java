a1ba3a7cffe84b754bcd3d0652432ebd4af4a9d2
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-19 15:48:00 -0300

    implementing basic authentication

--- awm-backend/src/main/java/com/stefano/api/awm/backend/repository/release/ReleaseRepositoryQuery.java
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/repository/release/ReleaseRepositoryQuery.java
@@ -1,7 +1,5 @@
 package com.stefano.api.awm.backend.repository.release;
 
-import java.util.List;
-
 import com.stefano.api.awm.backend.model.Release;
 import com.stefano.api.awm.backend.repository.filter.ReleaseFilter;
 

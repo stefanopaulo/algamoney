b5328412b6f13142e0e24d726a757a45828a7116
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-24 14:31:41 -0300

    configuring refresh token grant type on authorization server

--- awm-auth/src/main/java/com/algamoney/awm/auth/security/AuthorizationServerConfig.java
+++ awm-auth/src/main/java/com/algamoney/awm/auth/security/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package com.algamoney.awm.auth.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -15,10 +16,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	private final PasswordEncoder passwordEncoder;
 	private final AuthenticationManager authenticationManager;
+	private final UserDetailsService userDetailsService;
 
-	public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+	public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
 		this.passwordEncoder = passwordEncoder;
 		this.authenticationManager = authenticationManager;
+		this.userDetailsService = userDetailsService;
 	}
 	
 	@Override
@@ -27,23 +30,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.inMemory()
 				.withClient("angular")
 					.secret(passwordEncoder.encode("@ngul@r0"))
-					.authorizedGrantTypes("password")
+					.authorizedGrantTypes("password", "refresh_token")
 					.scopes("write", "read")
-					.accessTokenValiditySeconds(1800)
+					.accessTokenValiditySeconds(120)
+					.refreshTokenValiditySeconds(3600 * 24)
 			.and()
 				.withClient("mobile")
 					.secret("m0b1l3#")
-					.authorizedGrantTypes("password")
+					.authorizedGrantTypes("password", "refresh_token")
 					.scopes("write", "read")
-					.accessTokenValiditySeconds(1800)
+					.accessTokenValiditySeconds(120)
+					.refreshTokenValiditySeconds(3600 * 24)
 			.and()
 				.withClient("checktoken")
-					.secret("check123");
+					.secret(passwordEncoder.encode("check123"));
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService);
 	}
 	
 	@Override

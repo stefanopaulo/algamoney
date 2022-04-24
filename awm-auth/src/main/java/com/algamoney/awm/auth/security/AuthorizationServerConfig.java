92595cbb0ea3db8128582a0829905e545525aa09
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-24 11:17:44 -0300

    configuring token introspection endpoint on authorization server

--- awm-auth/src/main/java/com/algamoney/awm/auth/security/AuthorizationServerConfig.java
+++ awm-auth/src/main/java/com/algamoney/awm/auth/security/AuthorizationServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -40,6 +41,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("isAuthenticated()");
 	}
 
 }

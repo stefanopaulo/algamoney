79ef35e1389a18a0c124a159fafb1a3e80606cba
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-30 16:44:04 -0300

    Configuring Resource Server for Symmetric Key Signed JWT

--- awm-backend/src/main/java/com/stefano/api/awm/backend/config/security/ResourceServerConfig.java
+++ awm-backend/src/main/java/com/stefano/api/awm/backend/config/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.stefano.api.awm.backend.config.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +20,16 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
-				.oauth2ResourceServer().opaqueToken();
+				.cors()
+			.and()
+				.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		SecretKeySpec key = new SecretKeySpec("XwHzdhVFmEiPcFkq1pQ9xmjyoso23f1I".getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(key).build();
 	}
 
 }
